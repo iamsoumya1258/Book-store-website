8cb6e21b2154a03d2039a62a2402b6bb45884673
Soumya Ranjan Mahalik <iamsrm12@gmail.com>
2022-07-04 23:36:46 +0530

    initial commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra Links</h3>
+         <a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +91 7735578566 </p>
+         <p> <i class="fas fa-envelope"></i> iamsrm12@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Bhubaneswar, India - 751010 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.twitter.com/iamsrm12/"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/iamsrm_12/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/soumya-ranjan-mahalik-b289791a3/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>iamsrm12</span> </p>
+
+</section>
